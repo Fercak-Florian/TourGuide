dc283c4689907c522b3d3dbe2043c528cfb5e6ba
Fercak Florian <fercak.florian@hotmail.fr>
2023-03-10 14:53:34 +0100

    Javadoc écrite pour les classe UserService et RewardService

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
 import org.springframework.stereotype.Service;
 import gpsUtil.GpsUtil;
 import gpsUtil.location.Attraction;
@@ -13,71 +14,115 @@ import rewardCentral.RewardCentral;
 import tourGuide.user.User;
 import tourGuide.user.UserReward;
 
+/**
+ * This class is used by the TourGuideService class.
+ * This class contains several methods to manage user rewards
+ */
 @Service
 public class RewardsService {
-	private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
+    private static final double STATUTE_MILES_PER_NAUTICAL_MILE = 1.15077945;
 
-	// proximity in miles
-	private int defaultProximityBuffer = 10;
-	private int proximityBuffer = defaultProximityBuffer;
-	private int attractionProximityRange = 200;
-	private final GpsUtil GPS_UTIL;
-	private final RewardCentral REWARD_CENTRAL;
-	private Executor executor = Executors.newFixedThreadPool(100);
+    // proximity in miles
+    private int defaultProximityBuffer = 10;
+    private int proximityBuffer = defaultProximityBuffer;
+    private int attractionProximityRange = 200;
+    private final GpsUtil GPS_UTIL;
+    private final RewardCentral REWARD_CENTRAL;
+    private Executor executor = Executors.newFixedThreadPool(100);
 
-	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
-		this.GPS_UTIL = gpsUtil;
-		this.REWARD_CENTRAL = rewardCentral;
-	}
+    public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
+        this.GPS_UTIL = gpsUtil;
+        this.REWARD_CENTRAL = rewardCentral;
+    }
 
-	public void setProximityBuffer(int proximityBuffer) {
-		this.proximityBuffer = proximityBuffer;
-	}
+    /**
+     * This method is used to set the proximity buffer
+     *
+     * @param proximityBuffer object
+     */
+    public void setProximityBuffer(int proximityBuffer) {
+        this.proximityBuffer = proximityBuffer;
+    }
 
-	public void setDefaultProximityBuffer() {
-		proximityBuffer = defaultProximityBuffer;
-	}
+    /**
+     * This method is used to set the proximity buffer to default value
+     */
+    public void setDefaultProximityBuffer() {
+        proximityBuffer = defaultProximityBuffer;
+    }
 
-	public void calculateRewards(User user) {
-		List<VisitedLocation> userLocations = user.getVisitedLocations();
-		CompletableFuture.supplyAsync(() -> GPS_UTIL.getAttractions(), executor).thenAccept(attractions -> {
-			for (VisitedLocation visitedLocation : userLocations) {
-				for (Attraction attraction : attractions) {
-					if (user.getUserRewards().stream()
-							.filter(r -> r.ATTRACTION.attractionName.equals(attraction.attractionName)).count() == 0) {
-						if (nearAttraction(visitedLocation, attraction)) {
-							user.addUserReward(
-									new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-						}
-					}
-				}
-			}
-		});
-	}
+    /**
+     * This method calculates rewards for a user and contains ASYNCHRONOUS TECHNOLOGY
+     * When this method is called, GPS_UTIL.getAttractions() method is running into a separated thread by supplyAsync method
+     * which is waiting for the result. When the result of getAttractions() method id available,
+     * thenAccept method use it for next operations
+     *
+     * @param user of type User
+     */
+    public void calculateRewards(User user) {
+        List<VisitedLocation> userLocations = user.getVisitedLocations();
+        CompletableFuture.supplyAsync(() -> GPS_UTIL.getAttractions(), executor).thenAccept(attractions -> {
+            for (VisitedLocation visitedLocation : userLocations) {
+                for (Attraction attraction : attractions) {
+                    if (user.getUserRewards().stream()
+                            .filter(r -> r.ATTRACTION.attractionName.equals(attraction.attractionName)).count() == 0) {
+                        if (nearAttraction(visitedLocation, attraction)) {
+                            user.addUserReward(
+                                    new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+                        }
+                    }
+                }
+            }
+        });
+    }
 
-	private int getRewardPoints(Attraction attraction, User user) {
-		return REWARD_CENTRAL.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
-	}
+    /**
+     * This method get the user rewards points
+     *
+     * @param attraction object, user object
+     * @return int number
+     */
+    private int getRewardPoints(Attraction attraction, User user) {
+        return REWARD_CENTRAL.getAttractionRewardPoints(attraction.attractionId, user.getUserId());
+    }
 
-	public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
-		return getDistance(attraction, location) > attractionProximityRange ? false : true;
-	}
+    /**
+     * This method is used to define if a user is in the attraction perimeter
+     *
+     * @param attraction object, location object
+     * @return boolean
+     */
+    public boolean isWithinAttractionProximity(Attraction attraction, Location location) {
+        return getDistance(attraction, location) > attractionProximityRange ? false : true;
+    }
 
-	private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
-		return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
-	}
+    /**
+     * This method is used to define if a user has visited an attraction
+     *
+     * @param attraction object, location object
+     * @return boolean
+     */
+    private boolean nearAttraction(VisitedLocation visitedLocation, Attraction attraction) {
+        return getDistance(attraction, visitedLocation.location) > proximityBuffer ? false : true;
+    }
 
-	public double getDistance(Location loc1, Location loc2) {
-		double lat1 = Math.toRadians(loc1.latitude);
-		double lon1 = Math.toRadians(loc1.longitude);
-		double lat2 = Math.toRadians(loc2.latitude);
-		double lon2 = Math.toRadians(loc2.longitude);
+    /**
+     * This method is used to calculate the distance between two locations
+     *
+     * @param loc1 of Location type, loc2 of Location type
+     * @return double distance between this two locations
+     */
+    public double getDistance(Location loc1, Location loc2) {
+        double lat1 = Math.toRadians(loc1.latitude);
+        double lon1 = Math.toRadians(loc1.longitude);
+        double lat2 = Math.toRadians(loc2.latitude);
+        double lon2 = Math.toRadians(loc2.longitude);
 
-		double angle = Math
-				.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+        double angle = Math
+                .acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
 
-		double nauticalMiles = 60 * Math.toDegrees(angle);
-		double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
-		return statuteMiles;
-	}
+        double nauticalMiles = 60 * Math.toDegrees(angle);
+        double statuteMiles = STATUTE_MILES_PER_NAUTICAL_MILE * nauticalMiles;
+        return statuteMiles;
+    }
 }
--- TourGuide/src/main/java/tourGuide/service/TourGuideService.java
+++ TourGuide/src/main/java/tourGuide/service/TourGuideService.java
@@ -9,6 +9,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import gpsUtil.GpsUtil;
@@ -30,113 +31,118 @@ import tripPricer.TripPricer;
 @Service
 public class TourGuideService {
 
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	private final GpsUtil GPS_UTIL;
-	private final RewardCentral REWARD_CENTRAL;
-	private final RewardsService REWARD_SERVICE;
-	private final TripPricer TRIP_PRICER = new TripPricer();
-	public final Tracker tracker;
-	private static final String TRIP_PRICER_API_KEY = "test-server-api-key";
+    private final GpsUtil GPS_UTIL;
+    private final RewardCentral REWARD_CENTRAL;
+    private final RewardsService REWARD_SERVICE;
+    private final TripPricer TRIP_PRICER = new TripPricer();
+    public final Tracker tracker;
+    private static final String TRIP_PRICER_API_KEY = "test-server-api-key";
 
-	private Executor executor = Executors.newFixedThreadPool(100);
+    private Executor executor = Executors.newFixedThreadPool(100);
 
-	public TourGuideService(GpsUtil gpsUtil, RewardCentral rewardCentral, RewardsService rewardsService) {
-		this.GPS_UTIL = gpsUtil;
-		this.REWARD_CENTRAL = rewardCentral;
-		this.REWARD_SERVICE = rewardsService;
+    public TourGuideService(GpsUtil gpsUtil, RewardCentral rewardCentral, RewardsService rewardsService) {
+        this.GPS_UTIL = gpsUtil;
+        this.REWARD_CENTRAL = rewardCentral;
+        this.REWARD_SERVICE = rewardsService;
 
-		tracker = new Tracker(this);
-		addShutDownHook();
-	}
+        tracker = new Tracker(this);
+        addShutDownHook();
+    }
 
-	/**
-	 * This method gets the user GPS position
-	 * @Param a User object
-	 * @return a VisitedLocation containing the user id, the user Location and a date
-	 */
-	public VisitedLocation getUserLocation(User user) {
-		if (user.getVisitedLocations().isEmpty()) {
-			trackUserLocation(user);
-			return null;
-		} else {
-			return user.getLastVisitedLocation();
-		}
-	}
+    /**
+     * This method gets the user GPS position
+     *
+     * @return a VisitedLocation containing the user id, the user Location and a date
+     * @Param a User object
+     */
+    public VisitedLocation getUserLocation(User user) {
+        if (user.getVisitedLocations().isEmpty()) {
+            trackUserLocation(user);
+            return null;
+        } else {
+            return user.getLastVisitedLocation();
+        }
+    }
 
-	/**
-	 * This method gets all the most recent user GPS position
-	 *
-	 * @return a List of UserMostRecentLocation, each element of the list contains a user id
-	 * and the last user GPS position
-	 */
-	public List<UserMostRecentLocation> getAllUsersMostRecentLocation() {
-		List<User> users = userService.getAllUsers();
-		List<UserMostRecentLocation> usersMostRecentLocations = new ArrayList<>();
-		for (User user : users) {
-			usersMostRecentLocations
-					.add(new UserMostRecentLocation(user.getUserId(), user.getLastVisitedLocation().location));
-		}
-		return usersMostRecentLocations;
-	}
+    /**
+     * This method gets all the most recent user GPS position
+     *
+     * @return a List of UserMostRecentLocation, each element of the list contains a user id
+     * and the last user GPS position
+     */
+    public List<UserMostRecentLocation> getAllUsersMostRecentLocation() {
+        List<User> users = userService.getAllUsers();
+        List<UserMostRecentLocation> usersMostRecentLocations = new ArrayList<>();
+        for (User user : users) {
+            usersMostRecentLocations
+                    .add(new UserMostRecentLocation(user.getUserId(), user.getLastVisitedLocation().location));
+        }
+        return usersMostRecentLocations;
+    }
 
-	/**
-	 * This method gets the user trip deals
-	 *
-	 * @return a List of Provider, each element of the list contains a travel provider name,
-	 * the travel price and a trip id
-	 */
-	public List<Provider> getTripDeals(User user) {
-		int cumulativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
-		List<Provider> providers = TRIP_PRICER.getPrice(TRIP_PRICER_API_KEY, user.getUserId(),
-				user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
-				user.getUserPreferences().getTripDuration(), cumulativeRewardPoints);
-		user.setTripDeals(providers);
-		return providers;
-	}
+    /**
+     * This method gets the user trip deals
+     *
+     * @return a List of Provider, each element of the list contains a travel provider name,
+     * the travel price and a trip id
+     */
+    public List<Provider> getTripDeals(User user) {
+        int cumulativeRewardPoints = user.getUserRewards().stream().mapToInt(i -> i.getRewardPoints()).sum();
+        List<Provider> providers = TRIP_PRICER.getPrice(TRIP_PRICER_API_KEY, user.getUserId(),
+                user.getUserPreferences().getNumberOfAdults(), user.getUserPreferences().getNumberOfChildren(),
+                user.getUserPreferences().getTripDuration(), cumulativeRewardPoints);
+        user.setTripDeals(providers);
+        return providers;
+    }
 
-	/**
-	 * This method get the user most recent GPS position and calculate the user rewards
-	 *
-	 * @param user of type User
-	 */
-	public void trackUserLocation(User user) {
-		CompletableFuture.supplyAsync(() -> GPS_UTIL.getUserLocation(user.getUserId()), executor).thenAccept(v -> {
-			user.addToVisitedLocations(v);
-			REWARD_SERVICE.calculateRewards(user);
-		});
-	}
+    /**
+     * This method get the user most recent GPS position and calculate the user rewards
+     * This method contains ASYNCHRONOUS TECHNOLOGY
+     * When this method is called, GPS_UTIL.getUserLocation() method is running into a separated thread by supplyAsync method
+     * which is waiting for the result. When the result of getUserLocation() method id available,
+     * thenAccept method use it for next operations
+     *
+     * @param user of type User
+     */
+    public void trackUserLocation(User user) {
+        CompletableFuture.supplyAsync(() -> GPS_UTIL.getUserLocation(user.getUserId()), executor).thenAccept(v -> {
+            user.addToVisitedLocations(v);
+            REWARD_SERVICE.calculateRewards(user);
+        });
+    }
 
-	/**
-	 * This method gets the 5 closest attractions from the user GPS position
-	 *
-	 * @param visitedLocation
-	 * @return List of ProximityAttraction, each element of the list contains the attraction GPS position,
-	 * the GPS user position, the distance in miles between the user and the attraction,
-	 * the user rewards points and the attraction name.
-	 */
-	public List<ProximityAttraction> getNearByAttractions(VisitedLocation visitedLocation) {
-		Map<Attraction, Double> nearestAttractions = new HashMap<>();
+    /**
+     * This method gets the 5 closest attractions from the user GPS position
+     *
+     * @param visitedLocation
+     * @return List of ProximityAttraction, each element of the list contains the attraction GPS position,
+     * the GPS user position, the distance in miles between the user and the attraction,
+     * the user rewards points and the attraction name.
+     */
+    public List<ProximityAttraction> getNearByAttractions(VisitedLocation visitedLocation) {
+        Map<Attraction, Double> nearestAttractions = new HashMap<>();
 
-		for (Attraction attraction : GPS_UTIL.getAttractions()) {
-			nearestAttractions.put(attraction, REWARD_SERVICE.getDistance(visitedLocation.location, attraction));
-		}
-		return (nearestAttractions.entrySet().stream().sorted(Entry.comparingByValue()).limit(5)
-				.map(t -> new ProximityAttraction(t.getKey().attractionName, t.getKey().latitude, t.getKey().longitude,
-						visitedLocation.location, t.getValue(),
-						REWARD_CENTRAL.getAttractionRewardPoints(t.getKey().attractionId, visitedLocation.userId)))
-				.collect(Collectors.toList()));
-	}
+        for (Attraction attraction : GPS_UTIL.getAttractions()) {
+            nearestAttractions.put(attraction, REWARD_SERVICE.getDistance(visitedLocation.location, attraction));
+        }
+        return (nearestAttractions.entrySet().stream().sorted(Entry.comparingByValue()).limit(5)
+                .map(t -> new ProximityAttraction(t.getKey().attractionName, t.getKey().latitude, t.getKey().longitude,
+                        visitedLocation.location, t.getValue(),
+                        REWARD_CENTRAL.getAttractionRewardPoints(t.getKey().attractionId, visitedLocation.userId)))
+                .collect(Collectors.toList()));
+    }
 
-	/**
-	 * This method is used to stop the tracker running in a separated thread
-	 */
-	private void addShutDownHook() {
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
-				tracker.stopTracking();
-			}
-		});
-	}
+    /**
+     * This method is used to stop the tracker running in a separated thread
+     */
+    private void addShutDownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                tracker.stopTracking();
+            }
+        });
+    }
 }
