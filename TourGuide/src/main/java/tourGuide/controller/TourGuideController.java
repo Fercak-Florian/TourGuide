add02127f16de35a9f16ac1af08a570b1a749cd6
Fercak Florian <fercak.florian@hotmail.fr>
2023-03-10 12:14:43 +0100

    javadoc écrite pour la classe controller

--- TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
+++ TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
@@ -1,7 +1,7 @@
 package tourGuide.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,56 +10,99 @@ import gpsUtil.location.VisitedLocation;
 import tourGuide.service.TourGuideService;
 import tourGuide.service.UserService;
 import tourGuide.user.User;
+import tourGuide.user.UserReward;
 import tourGuide.utils.ProximityAttraction;
 import tourGuide.utils.UserMostRecentLocation;
 import tripPricer.Provider;
 
+/**
+ * This class contains the endpoints called by the front-end API of TourGuide.
+ * It allows to obtain information about the user such as his position, his places visited, his rewards.
+ */
 @RestController
 public class TourGuideController {
-	TourGuideService tourGuideService;
-	UserService userService;
+    TourGuideService tourGuideService;
+    UserService userService;
 
-	public TourGuideController(TourGuideService tourGuideService, UserService userService){
-		this.tourGuideService = tourGuideService;
-		this.userService = userService;
-	}
+    public TourGuideController(TourGuideService tourGuideService, UserService userService) {
+        this.tourGuideService = tourGuideService;
+        this.userService = userService;
+    }
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from TourGuide!";
-	}
+    /**
+     * This method gets a welcome message
+     *
+     * @return a String containing the welcome message
+     */
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from TourGuide!";
+    }
 
-	@GetMapping("/getLocation")
-	public String getLocation(@RequestParam String userName) {
-		VisitedLocation visitedLocation = tourGuideService.getUserLocation(userService.getUser(userName));
-		if(visitedLocation == null) {
-			return "{}";
-		} else {
-			return JsonStream.serialize(visitedLocation.location);
-		}
-	}
+    /**
+     * This method gets the user GPS position
+     *
+     * @param userName of type String
+     * @return a String with the latitude and longitude
+     */
+    @GetMapping("/getLocation")
+    public String getLocation(@RequestParam String userName) {
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(userService.getUser(userName));
+        if (visitedLocation == null) {
+            return "{}";
+        } else {
+            return JsonStream.serialize(visitedLocation.location);
+        }
+    }
 
-	@GetMapping("/getNearbyAttractions")
-	public List<ProximityAttraction> getNearbyAttractions(@RequestParam String userName) {
-		VisitedLocation visitedLocation = tourGuideService.getUserLocation(userService.getUser(userName));
-		return tourGuideService.getNearByAttractions(visitedLocation);
-	}
+    /**
+     * This method gets the 5 closest attractions from the user GPS position
+     *
+     * @param userName of type String
+     * @return List of ProximityAttraction, each element of the list contains the attraction GPS position,
+     * the GPS user position, the distance in miles between the user and the attraction,
+     * the user rewards points and the attraction name.
+     */
+    @GetMapping("/getNearbyAttractions")
+    public List<ProximityAttraction> getNearbyAttractions(@RequestParam String userName) {
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(userService.getUser(userName));
+        return tourGuideService.getNearByAttractions(visitedLocation);
+    }
 
-	@GetMapping("/getRewards")
-	public String getRewards(@RequestParam String userName) {
-		User user = userService.getUser(userName);
-		return JsonStream.serialize(user.getUserRewards());
+    /**
+     * This method gets the user rewards
+     *
+     * @param userName of type String
+     * @return a List of UserRewards, each element of the list contains a visitedLocation,
+     * an attraction and rewards points.
+     */
+    @GetMapping("/getRewards")
+    public List<UserReward> getRewards(@RequestParam String userName) {
+        User user = userService.getUser(userName);
+        return (user.getUserRewards());
 
-	}
+    }
 
-	@GetMapping("/getAllCurrentLocations")
-	public List<UserMostRecentLocation> getAllCurrentLocations() {
-		return tourGuideService.getAllUsersMostRecentLocation();
-	}
+    /**
+     * This method gets all the most recent user GPS position
+     *
+     * @return a List of UserMostRecentLocation, each element of the list contains a user id
+     * and the last user GPS position
+     */
+    @GetMapping("/getAllCurrentLocations")
+    public List<UserMostRecentLocation> getAllCurrentLocations() {
+        return tourGuideService.getAllUsersMostRecentLocation();
+    }
 
-	@GetMapping("/getTripDeals")
-	public String getTripDeals(@RequestParam String userName) {
-		List<Provider> providers = tourGuideService.getTripDeals(userService.getUser(userName));
-		return JsonStream.serialize(providers);
-	}
+    /**
+     * This method gets the user trip deals
+     *
+     * @param userName of type String
+     * @return a List of Provider, each element of the list contains a travel provider name,
+     * the travel price and a trip id
+     */
+    @GetMapping("/getTripDeals")
+    public List<Provider> getTripDeals(@RequestParam String userName) {
+        return tourGuideService.getTripDeals(userService.getUser(userName));
+    }
 }
